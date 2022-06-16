0e816bf5adf40c04541efecaca040544b561cd3e
id-aaryan <am5020@imperial.ac.uk>
2022-06-16 15:05:12 +0100

    change path name

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const storage = multer.diskStorage({
         cb(null, 'uploads')
     },
     filename: (req, file, cb) => {
-        cb(null, file.fieldname + '-' + Date.now())
+        cb(null, file.fieldname + '-' + Date.now()+path.extname)
     }
 });
 
